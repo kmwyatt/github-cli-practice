2d4858a81bb240c6cab36d376bcf9c9979b1c5ed
kmwyatt <arc2437@naver.com>
2022-10-02 21:42:46 +0900

    add commander

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+const { program } = require('commander');
+
+program.version('0.0.1');
+
+program
+    .command('list-bugs')
+    .description('List issues with bug label')
+    .action(async () => {
+        console.log('List bugs!');
+    });
+
+program
+    .command('check-prs')
+    .description('Check pull request status')
+    .action(async () => {
+        console.log('Check PRs!');
+    });
+
+program.parseAsync();
