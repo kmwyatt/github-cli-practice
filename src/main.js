930964db93204c33f2c9a5756fd3d22855e976a8
kmwyatt <arc2437@naver.com>
2022-10-03 20:34:06 +0900

    add too-big label to PR that has changes more than 100 lines

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ program.version('0.0.1');
 
 const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
 
+const OWNER = 'kmwyatt';
+const REPO = 'github-cli-practice';
+const LABEL_TOO_BIG = 'too-big';
+
 program
     .command('me')
     .description('Check my profile')
@@ -26,8 +30,8 @@ program
     .description('List issues with bug label')
     .action(async () => {
         const result = await octokit.rest.issues.listForRepo({
-            owner: 'kmwyatt',
-            repo: 'github-cli-practice',
+            owner: OWNER,
+            repo: REPO,
         });
 
         // console.log(result);
@@ -54,7 +58,55 @@ program
     .command('check-prs')
     .description('Check pull request status')
     .action(async () => {
-        console.log('Check PRs!');
+        const result = await octokit.rest.pulls.list({
+            owner: OWNER,
+            repo: REPO,
+        });
+
+        const prsWithDiff = await Promise.all(
+            result.data.map(async (pr) => ({
+                labels: pr.labels,
+                number: pr.number,
+                compare: await octokit.rest.repos.compareCommits({
+                    owner: OWNER,
+                    repo: REPO,
+                    base: pr.base.ref,
+                    head: pr.head.ref,
+                }),
+            })),
+        );
+
+        await Promise.all(
+            prsWithDiff
+                .map(({ compare, ...rest }) => {
+                    const totalChanges = compare.data.files.reduce(
+                        (sum, file) => sum + file.changes,
+                        0,
+                    );
+                    return {
+                        compare,
+                        totalChanges,
+                        ...rest,
+                    };
+                })
+                .filter((pr) => pr.totalChanges > 100)
+                .map(async ({ labels, number, totalChanges }) => {
+                    console.log(
+                        `PR #${number}, Total Changes: ${totalChanges}`,
+                    );
+                    if (!labels.find((label) => label.name === LABEL_TOO_BIG)) {
+                        console.log(
+                            `Adding ${LABEL_TOO_BIG} label to PR #${number}...`,
+                        );
+                        return octokit.rest.issues.addLabels({
+                            owner: OWNER,
+                            repo: REPO,
+                            issue_number: number,
+                            labels: [LABEL_TOO_BIG],
+                        });
+                    }
+                }),
+        );
     });
 
 program.parseAsync();
